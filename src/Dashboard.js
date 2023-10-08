b969a83b36b54f2e55e9c6f663049790f0b6e24c
maxibarra <64232276+maxibarra@users.noreply.github.com>
2023-10-08 18:04:27 -0300

    descarga de recibos
    se puede descargar el recibo pero hay que encontrar como hacerlo dinamico para cada usuario

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -33,25 +33,44 @@ function Dashboard() {
     fetchUserName(); // eslint-disable-next-line
   }, [user, loading]);
 
+  // const bajar = async () => {
+  //   try {
+  //     getDownloadURL(ref(storage, `gs://login-rutabus.appspot.com/Recibos/350-SUE-M-2307-STT-03-ADMINISTRACION-00000501-20231006-125445-USR18.pdf`))
+  //       .then((url) => {
+  //         const xhr = new XMLHttpRequest();
+  //         xhr.responseType = "blob";
+  //         xhr.onload = (event) => {
+  //           // eslint-disable-next-line
+  //           const blob = xhr.response;
+  //         };
+  //         xhr.open("GET", url);
+  //         xhr.send();
+  //       })
+  //       .catch((error) => {
+  //         alert(`error:${error}`);
+  //       });
+  //   } catch (err) {
+  //     alert(`error:${err}`);
+  //   }
+  // };
   const bajar = async () => {
-    try {
-      getDownloadURL(ref(storage, `gs://login-rutabus.appspot.com/Recibos/350-SUE-M-2307-STT-03-ADMINISTRACION-00000501-20231006-125445-USR18.pdf`))
-        .then((url) => {
-          const xhr = new XMLHttpRequest();
-          xhr.responseType = "blob";
-          xhr.onload = (event) => {
-            // eslint-disable-next-line
-            const blob = xhr.response;
-          };
-          xhr.open("GET", url);
-          xhr.send();
-        })
-        .catch((error) => {
-          alert(`error:${error}`);
-        });
-    } catch (err) {
-      alert(`error:${err}`);
-    }
+
+    const legajoUsuario =`${leg}`;
+    try{
+    const url = await getDownloadURL(ref(storage, `gs://login-rutabus.appspot.com/Recibos/350-SUE-M-2309-STT-03-ADMINISTRACION-00000${legajoUsuario}-20231006-125638-USR18.pdf`));
+      //hay que ver de que manera cambiar el mes del recibo y los ultimos 6 numeros que nose de donde vienen
+      
+    // Crear un enlace de descarga invisible en el DOM
+    const downloadLink = document.createElement("a");
+    downloadLink.href = url;
+    downloadLink.download = "archivo.pdf"; // Nombre que se utilizará para guardar el archivo
+
+    // Simular un clic en el enlace para iniciar la descarga
+    downloadLink.click();
+    window.open(url, '_blank')
+  }catch(error){
+    console.log(`Error, ${error}`);
+  }
   };
   return (
     <div className="dashboard">
